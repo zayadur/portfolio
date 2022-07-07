e9945c885d7fe3d524d8bd7e12476a4a904d022a
zayadur <zayadur@outlook.com>
2022-07-07 13:09:49 -0400

    Rename className to class in ProjectList (avoid merge conflict)

--- components/ProjectList.jsx
+++ components/ProjectList.jsx
@@ -3,11 +3,11 @@ import Image from 'next/image';
 
 export default function ProjectList({ projectData }) {
   return (
-    <section className="mt-6 space-y-12 lg:space-y-0 lg:grid lg:grid-cols-3 lg:gap-x-6">
+    <section class="mt-6 space-y-12 lg:space-y-0 lg:grid lg:grid-cols-3 lg:gap-x-6">
       {projectData.map(({ id, title, stack }) => (
-        <div key={id} className="group relative">
-          <div className="relative w-full h-90 rounded-lg group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-90 lg:aspect-w-1 lg:aspect-h-1 cursor-pointer ease-in-out duration-300">
-            <p className="font-bold my-3 text-3xl text-center cursor-pointer">{title}</p>
+        <div key={id} class="group relative">
+          <div class="relative w-full h-90 rounded-lg group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-90 lg:aspect-w-1 lg:aspect-h-1 cursor-pointer ease-in-out duration-300">
+            <p class="font-bold my-3 text-3xl text-center cursor-pointer">{title}</p>
             <Link href={`/projects/${id}`}>
               <Image
                 src={'/images/projects/' + id + '.png'}
@@ -16,7 +16,7 @@ export default function ProjectList({ projectData }) {
                 height="900"
               />
             </Link>
-            <p className="text-2xl text-center">{stack}</p>
+            <p class="text-2xl text-center">{stack}</p>
           </div>
         </div>
       ))}
