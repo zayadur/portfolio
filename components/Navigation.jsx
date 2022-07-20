15fe74b20fd541d8ce93c9b38eb7b3f1d214ab58
zayadur <zayadur@outlook.com>
2022-07-20 16:51:16 -0400

    Refactor components with className

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -6,19 +6,19 @@ import { faGithub, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-ic
 
 export default function Navigation() {
   return (
-    <nav class="mb-6">
-      <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-        <div class="relative flex items-center justify-between h-16">
-          <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+    <nav className="mb-6">
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div className="relative flex items-center justify-between h-16">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             {/* Mobile menu button*/}
-            <button type="button" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-              <span class="sr-only">Open main menu</span>
+            <button type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+              <span className="sr-only">Open main menu</span>
               {/*
                 Icon when menu is closed.
                 Heroicon name: outline/menu
                 Menu open: "hidden", Menu closed: "block"
               */}
-              <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+              <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
               </svg>
               {/*
@@ -26,13 +26,13 @@ export default function Navigation() {
                 Heroicon name: outline/x
                 Menu open: "block", Menu closed: "hidden"
               */}
-              <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+              <svg className="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
               </svg>
             </button>
           </div>
-          <div class="ml-3 flex-1 flex items-center justify-start sm:items-stretch sm:justify-start">
-            <div class="flex-shrink-0 flex items-center">
+          <div className="ml-3 flex-1 flex items-center justify-start sm:items-stretch sm:justify-start">
+            <div className="flex-shrink-0 flex items-center">
               <Image
                 className={'block lg:block h-8 w-auto rounded-full'}
                 src={'/images/profile.jpg'}
@@ -41,27 +41,27 @@ export default function Navigation() {
                 alt='Profile picture'
               />
             </div>
-            <div class="hidden sm:block sm:ml-6">
-              <div class="flex space-x-4">
+            <div className="hidden sm:block sm:ml-6">
+              <div className="flex space-x-4">
                 {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
-                <a href="/" class="text-white hover:bg-neutral-900 px-3 py-2 rounded-md text-md font-bold ease-in-out duration-200">about</a>
-                <a href="/projects" class="text-white hover:bg-neutral-900 px-3 py-2 rounded-md text-md font-bold ease-in-out duration-200">projects</a>
-                <a href="/resume.pdf" target={"_blank"} class="text-white hover:bg-neutral-900 px-3 py-2 rounded-md text-md font-bold ease-in-out duration-200">resume</a>
-                <a href="mailto:zayadur@outlook.com" class="text-white hover:bg-neutral-900 px-3 py-2 rounded-md text-md font-bold ease-in-out duration-200">contact</a>
+                <a href="/" className="text-white hover:bg-neutral-900 px-3 py-2 rounded-md text-md font-bold ease-in-out duration-200">about</a>
+                <a href="/projects" className="text-white hover:bg-neutral-900 px-3 py-2 rounded-md text-md font-bold ease-in-out duration-200">projects</a>
+                <a href="/resume.pdf" target={"_blank"} className="text-white hover:bg-neutral-900 px-3 py-2 rounded-md text-md font-bold ease-in-out duration-200">resume</a>
+                <a href="mailto:zayadur@outlook.com" className="text-white hover:bg-neutral-900 px-3 py-2 rounded-md text-md font-bold ease-in-out duration-200">contact</a>
               </div>
             </div>
           </div>
-          <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            <a href="https://github.com/zayadur" target="_blank" class="p-2 rounded-full text-white hover:text-neutral-400 ease-in-out duration-200">
-              <span class="sr-only">GitHub</span>
+          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            <a href="https://github.com/zayadur" target="_blank" className="p-2 rounded-full text-white hover:text-neutral-400 ease-in-out duration-200">
+              <span className="sr-only">GitHub</span>
               <FontAwesomeIcon className='text-2xl' icon={faGithub}></FontAwesomeIcon>
             </a>
-            <a href="https://www.linkedin.com/in/zayadur" target="_blank" class="p-2 rounded-full text-white hover:text-neutral-400 ease-in-out duration-200">
-              <span class="sr-only">LinkedIn</span>
+            <a href="https://www.linkedin.com/in/zayadur" target="_blank" className="p-2 rounded-full text-white hover:text-neutral-400 ease-in-out duration-200">
+              <span className="sr-only">LinkedIn</span>
               <FontAwesomeIcon className='text-2xl' icon={faLinkedin}></FontAwesomeIcon>
             </a>
-            <a href="https://twitter.com/zayadur" target="_blank" class="p-2 rounded-full text-white hover:text-neutral-400 ease-in-out duration-200">
-              <span class="sr-only">Twitter</span>
+            <a href="https://twitter.com/zayadur" target="_blank" className="p-2 rounded-full text-white hover:text-neutral-400 ease-in-out duration-200">
+              <span className="sr-only">Twitter</span>
               <FontAwesomeIcon className='text-2xl' icon={faTwitter}></FontAwesomeIcon>
             </a>
           </div>
@@ -69,13 +69,13 @@ export default function Navigation() {
       </div>
 
       {/* Mobile menu, show/hide based on menu state. */}
-      <div class="sm:hidden" id="mobile-menu">
-        <div class="flex justify-center">
+      <div className="sm:hidden" id="mobile-menu">
+        <div className="flex justify-center">
           {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
-          <a href="/" class="text-white hover:bg-neutral-900 px-3 py-2 rounded-md text-md font-semibold">about</a>
-          <a href="/projects" class="text-white hover:bg-neutral-900 px-3 py-2 rounded-md text-md font-semibold">projects</a>
-          <a href="/resume.pdf" target={"_blank"} class="text-white hover:bg-neutral-900 px-3 py-2 rounded-md text-md font-semibold">resume</a>
-          <a href="mailto:zayadur@outlook.com" class="text-white hover:bg-neutral-900 px-3 py-2 rounded-md text-md font-semibold">contact</a>
+          <a href="/" className="text-white hover:bg-neutral-900 px-3 py-2 rounded-md text-md font-semibold">about</a>
+          <a href="/projects" className="text-white hover:bg-neutral-900 px-3 py-2 rounded-md text-md font-semibold">projects</a>
+          <a href="/resume.pdf" target={"_blank"} className="text-white hover:bg-neutral-900 px-3 py-2 rounded-md text-md font-semibold">resume</a>
+          <a href="mailto:zayadur@outlook.com" className="text-white hover:bg-neutral-900 px-3 py-2 rounded-md text-md font-semibold">contact</a>
         </div>
       </div>
     </nav>
--- components/ProjectList.jsx
+++ components/ProjectList.jsx
@@ -4,12 +4,12 @@ import Image from 'next/image';
 export default function ProjectList({ projectData }) {
   return (
     <div>
-      <h1 class="my-6 flex justify-center text-4xl font-black">software</h1>
-      <section class="my-6 space-y-12 lg:space-y-0 lg:grid lg:grid-cols-3 lg:gap-x-6">
+      <h1 className="my-6 flex justify-center text-4xl font-black">software</h1>
+      <section className="my-6 space-y-12 lg:space-y-0 lg:grid lg:grid-cols-3 lg:gap-x-6">
         {projectData.map(({ id, title, stack }) => (
-          <div key={id} class="group relative">
-            <div class="relative w-full h-90 rounded-lg group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-90 lg:aspect-w-1 lg:aspect-h-1 cursor-pointer ease-in-out duration-200">
-              <p class="font-bold my-3 text-2xl text-center cursor-pointer">{title}</p>
+          <div key={id} className="group relative">
+            <div className="relative w-full h-90 rounded-lg group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-90 lg:aspect-w-1 lg:aspect-h-1 cursor-pointer ease-in-out duration-200">
+              <p className="font-bold my-3 text-2xl text-center cursor-pointer">{title}</p>
               <Link href={`/projects/${id}`}>
                 <Image
                   src={'/images/projects/' + id + '.png'}
@@ -18,17 +18,17 @@ export default function ProjectList({ projectData }) {
                   height='720'
                 />
               </Link>
-              <p class="text-lg text-center">{stack}</p>
+              <p className="text-lg text-center">{stack}</p>
             </div>
           </div>
         ))}
       </section>
       <hr />
-      <h1 class="my-6 flex justify-center text-4xl font-black">video</h1>
-      <section class="my-6 space-y-12 lg:space-y-0 lg:grid lg:grid-cols-4 lg:gap-x-6">
-        <div class="group relative">
-          <div class="relative w-full h-90 rounded-lg group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-90 lg:aspect-w-1 lg:aspect-h-1 cursor-pointer ease-in-out duration-200">
-            <p class="font-bold my-3 text-2xl text-center cursor-pointer">Volume Warning</p>
+      <h1 className="my-6 flex justify-center text-4xl font-black">video</h1>
+      <section className="my-6 space-y-12 lg:space-y-0 lg:grid lg:grid-cols-4 lg:gap-x-6">
+        <div className="group relative">
+          <div className="relative w-full h-90 rounded-lg group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-90 lg:aspect-w-1 lg:aspect-h-1 cursor-pointer ease-in-out duration-200">
+            <p className="font-bold my-3 text-2xl text-center cursor-pointer">Volume Warning</p>
             <a href='https://youtu.be/548slgzY2x0' target="_blank">
               <Image
                 src={'/images/projects/VolumeWarning.png'}
@@ -40,9 +40,9 @@ export default function ProjectList({ projectData }) {
             </a>
           </div>
         </div>
-        <div class="group relative">
-          <div class="relative w-full h-90 rounded-lg group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-90 lg:aspect-w-1 lg:aspect-h-1 cursor-pointer ease-in-out duration-200">
-            <p class="font-bold my-3 text-2xl text-center cursor-pointer">Let's Do It</p>
+        <div className="group relative">
+          <div className="relative w-full h-90 rounded-lg group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-90 lg:aspect-w-1 lg:aspect-h-1 cursor-pointer ease-in-out duration-200">
+            <p className="font-bold my-3 text-2xl text-center cursor-pointer">Let's Do It</p>
             <a href='https://youtu.be/ApPU5iHVHC8' target='_blank'>
               <Image
                 src={'/images/projects/LetsDoIt.png'}
@@ -53,9 +53,9 @@ export default function ProjectList({ projectData }) {
             </a>
           </div>
         </div>
-        <div class="group relative">
-          <div class="relative w-full h-90 rounded-lg group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-90 lg:aspect-w-1 lg:aspect-h-1 cursor-pointer ease-in-out duration-200">
-            <p class="font-bold my-3 text-2xl text-center cursor-pointer">Great Value Trash Bag</p>
+        <div className="group relative">
+          <div className="relative w-full h-90 rounded-lg group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-90 lg:aspect-w-1 lg:aspect-h-1 cursor-pointer ease-in-out duration-200">
+            <p className="font-bold my-3 text-2xl text-center cursor-pointer">Great Value Trash Bag</p>
             <a href='https://youtu.be/l8UNAGTx_ZU' target='_blank'>
               <Image
                 src={'/images/projects/GreatValueTrashBag.png'}
@@ -66,9 +66,9 @@ export default function ProjectList({ projectData }) {
             </a>
           </div>
         </div>
-        <div class="group relative">
-          <div class="relative w-full h-90 rounded-lg group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-90 lg:aspect-w-1 lg:aspect-h-1 cursor-pointer ease-in-out duration-200">
-            <p class="font-bold my-3 text-2xl text-center cursor-pointer">NYAN</p>
+        <div className="group relative">
+          <div className="relative w-full h-90 rounded-lg group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-90 lg:aspect-w-1 lg:aspect-h-1 cursor-pointer ease-in-out duration-200">
+            <p className="font-bold my-3 text-2xl text-center cursor-pointer">NYAN</p>
             <a href='https://youtu.be/WOmurGPQ9EA' target='_blank'>
               <Image
                 src={'/images/projects/Nyan.png'}
